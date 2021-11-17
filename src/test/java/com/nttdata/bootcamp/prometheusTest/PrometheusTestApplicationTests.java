5e21dab9a8318157998f74ccbf89b785e541b525
rafliefue <rafliefue@gmail.com>
2021-11-17 09:49:08 +0100

    añadiendo prometheus

---
+++ src/test/java/com/nttdata/bootcamp/prometheusTest/PrometheusTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.prometheusTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
